f02e06e3dd424df886904f24b719c1364c9c9835
TIPQC <TIPQC>
2017-04-21 09:48:02 +0800

    working next and back buttons

--- app/src/main/java/cs33fc1/ite010/tipqc/com/labactivities/PersonalInfoActivity1.java
+++ app/src/main/java/cs33fc1/ite010/tipqc/com/labactivities/PersonalInfoActivity1.java
@@ -1,13 +1,33 @@
 package cs33fc1.ite010.tipqc.com.labactivities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PersonalInfoActivity1 extends AppCompatActivity {
+
+    private Button btnNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.personal_info_p1);
+
+        btnNext = (Button) findViewById(R.id.btn_next);
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchPage2();
+            }
+        });
+
     }
+
+    private void launchPage2() {
+        Intent intent = new Intent(this, PersonalInfoActivity2.class);
+        startActivity(intent);
+    }
+
 }
--- app/src/main/java/cs33fc1/ite010/tipqc/com/labactivities/PersonalInfoActivity2.java
+++ app/src/main/java/cs33fc1/ite010/tipqc/com/labactivities/PersonalInfoActivity2.java
@@ -2,13 +2,27 @@ package cs33fc1.ite010.tipqc.com.labactivities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PersonalInfoActivity2 extends AppCompatActivity {
+
+    private Button btnPrevious;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.personal_info_p2);
+
+        btnPrevious = (Button) findViewById(R.id.btn_previous);
+
+        btnPrevious.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
     }
-    
+
 }
